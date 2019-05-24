64195fbd608f77accb9cc03cf975bb127ca2030d
Miné E <mine_esen@yahoo.ca>
2019-05-24 20:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 
 function deleteFromObjectByKey(object,key){
   var newObject1 = Object.assign({},object,key);
+<<<<<<< HEAD
   delete newObject1[key];
   return newObject1;
 }
@@ -18,4 +19,8 @@ function deleteFromObjectByKey(object,key){
 function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key];
   return object;
+=======
+  delete newObject1.key;
+  return newObject1;
+>>>>>>> 0450d4a8710a00e4dd54be85c76e0aab83720b9d
 }
